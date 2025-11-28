214b6b636a45ccb1038bf0c3fd0066f72ff1979f
Gwiz65 <gwiz2009@gmail.com>
2025-11-27 22:42:12 -0500

    spirit and tower guards can attack uniques

--- org/gwiz/wurmunlimited/mods/GwizServerTweaks.java
+++ org/gwiz/wurmunlimited/mods/GwizServerTweaks.java
@@ -5,7 +5,7 @@
  * distribute this software, either in source code form or as a compiled
  * binary, for any purpose, commercial or non-commercial, and by any
  * means.
- * 
+ *
  * In jurisdictions that recognize copyright laws, the author or authors
  * of this software dedicate any and all copyright interest in the
  * software to the public domain. We make this dedication for the benefit
@@ -13,7 +13,7 @@
  * successors. We intend this dedication to be an overt act of
  * relinquishment in perpetuity of all present and future rights to this
  * software under copyright law.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -21,7 +21,7 @@
  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- * 
+ *
  * For more information, please refer to <http://unlicense.org/>
 */
 
@@ -36,6 +36,7 @@ import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
 import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
 import org.gotti.wurmunlimited.modloader.interfaces.Versioned;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
+
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -45,13 +46,17 @@ import javassist.expr.MethodCall;
 
 public class GwizServerTweaks implements WurmServerMod, Configurable, PreInitable, Versioned {
 
-	private static final String version = "0.65";
+	private static final String version = "0.70";
 	private static Logger logger = Logger.getLogger(GwizServerTweaks.class.getName());
 	private static boolean allowInterfaithLink = true;
+	private static boolean spiritGuardsTargetUniques = true;
+	private static boolean towerGuardsTargetUniques = true;
 
 	@Override
 	public void configure(Properties properties) {
 		allowInterfaithLink = Boolean.parseBoolean(properties.getProperty("allowInterfaithLink", "true"));
+		spiritGuardsTargetUniques = Boolean.parseBoolean(properties.getProperty("spiritGuardsTargetUniques", "true"));
+		towerGuardsTargetUniques = Boolean.parseBoolean(properties.getProperty("towerGuardsTargetUniques", "true"));
 	}
 
 	@Override
@@ -69,6 +74,7 @@ public class GwizServerTweaks implements WurmServerMod, Configurable, PreInitabl
 				CtClass ctCreatureBehaviour = hookClassPool
 						.getCtClass("com.wurmonline.server.behaviours.CreatureBehaviour");
 				ctCreatureBehaviour.getDeclaredMethod("handle_MAGICLINK").instrument(new ExprEditor() {
+					@Override
 					public void edit(MethodCall methodCall) throws CannotCompileException {
 						if (methodCall.getMethodName().equals("getTemplateDeity")) {
 							methodCall.replace("{ $_ = 0; }");
@@ -80,6 +86,58 @@ public class GwizServerTweaks implements WurmServerMod, Configurable, PreInitabl
 				logger.log(Level.WARNING, "Something went horribly wrong when allowing inter-faith linking!", e);
 			}
 		}
-	}
 
+		// Make spirit guards attack unique creatures
+		if (spiritGuardsTargetUniques) {
+			try {
+				CtClass ctVillage = hookClassPool.getCtClass("com.wurmonline.server.villages.Village");
+				ctVillage.getDeclaredMethod("isEnemy", new CtClass[] {
+						hookClassPool.getCtClass("com.wurmonline.server.creatures.Creature"), CtClass.booleanType })
+						.instrument(new ExprEditor() {
+
+							@Override
+							public void edit(MethodCall methodCall) throws CannotCompileException {
+								if (methodCall.getMethodName().equals("isUnique")) {
+									methodCall.replace("{ $_ = false; }");
+								}
+							}
+						});
+				ctVillage
+						.getDeclaredMethod("addTarget",
+								new CtClass[] { hookClassPool.getCtClass("com.wurmonline.server.creatures.Creature") })
+						.instrument(new ExprEditor() {
+							@Override
+							public void edit(MethodCall methodCall) throws CannotCompileException {
+								if (methodCall.getMethodName().equals("isUnique")) {
+									methodCall.replace("{ $_ = false; }");
+								}
+							}
+						});
+				logger.log(Level.INFO, "Spirit guards will now attack unique creatures.");
+			} catch (NotFoundException | CannotCompileException e) {
+				logger.log(Level.WARNING, "Something went horribly wrong allowing spirit guards to target uniques!", e);
+			}
+		}
+
+		// Make tower guards attack unique creatures
+		if (towerGuardsTargetUniques) {
+			try {
+				CtClass ctGuardTower = hookClassPool.getCtClass("com.wurmonline.server.kingdom.GuardTower");
+				ctGuardTower
+						.getDeclaredMethod("alertGuards",
+								new CtClass[] { hookClassPool.getCtClass("com.wurmonline.server.creatures.Creature") })
+						.instrument(new ExprEditor() {
+							@Override
+							public void edit(MethodCall methodCall) throws CannotCompileException {
+								if (methodCall.getMethodName().equals("getAttitude")) {
+									methodCall.replace("{ if ($0.isUnique()) $_ = 2; else $_ = $proceed($$); }");
+								}
+							}
+						});
+				logger.log(Level.INFO, "Tower guards will now attack unique creatures.");
+			} catch (NotFoundException | CannotCompileException e) {
+				logger.log(Level.WARNING, "Something went horribly wrong allowing tower guards to target uniques!", e);
+			}
+		}
+	}
 }
